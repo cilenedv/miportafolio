e872f1eeac0f2d46259883dcf4b0b7e946f3874a
cilenedv <cilenedelacruz@gmail.com>
2016-11-27 19:35:54 -0500

    arreglando portada

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-$(".tl").textillate({
- in:{effect:'fadeIn',sequence:100},
- out:{effect:'fadeOutDown',shuffle:true,delay:1},
- loop:true
-});
 
 $(".tlt").textillate({
  in:{effect:'fadeInLeft',delay:20},
@@ -10,8 +5,3 @@ $(".tlt").textillate({
  loop:true
 });
 
-$(".aboutmeh1").textillate({
- in:{effect:'RotateIn',delay:20},
- out:{effect:'rollOut',sync:true,delay:1},
- loop:true
-});
