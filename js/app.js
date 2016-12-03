3be0584ee404ec911910d60e715f60bb2a715a79
cilenedv <cilenedelacruz@gmail.com>
2016-12-03 15:03:52 -0500

    arreglando imagenes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 
+(function() {
+var navLinks = $('nav ul li a'),
+    navH = $('nav').height(),
+    section = $('section'),
+    documentEl = $(document);
+    
+documentEl.on('scroll', function() {
+    var currentScrollPos = documentEl.scrollTop();
+    
+    section.each(function() {
+        var self = $(this);
+        if ( self.offset().top < (currentScrollPos + navH) && (currentScrollPos + navH) < (self.offset().top + self.outerHeight()) ) {
+            var targetClass = '.' + self.attr('id') + '-marker';
+            navLinks.removeClass('active');
+            $(targetClass).addClass('active');
+        }
+    });   
+});
+})();
 $(".tlt").textillate({
  in:{effect:'fadeInLeft',delay:20},
  out:{effect:'swing',sync:true,delay:1},
  loop:true
 });
+
 
